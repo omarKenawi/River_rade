231a70a5d0aab7b9d27ef4d53e1a3e9ec8e8fc00
omar <202027471@std.sci.cu.edu.eg>
2022-12-23 16:12:10 +0200

    change plane speed

--- src/Textures/Example1/plane1.java
+++ src/Textures/Example1/plane1.java
@@ -4,7 +4,7 @@ public class plane1 {
     private final String firstPic="plane_1.png";
     private final String secendPic="plane_2.png";
     private final String triedPic="plane_3.png";
-    private final double planeSpeed = .2;
+    private final double planeSpeed = 3;
 
     private final   String planeBoomed="plane_boom.png";
 
