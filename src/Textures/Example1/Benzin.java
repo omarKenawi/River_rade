1acdb684878c0be5d919a2059020ca653508818b
aliyasser <oy36578@gmail.com>
2022-12-25 21:04:41 +0200

    class full

---
+++ src/Textures/Example1/Benzin.java
@@ -0,0 +1,28 @@
+package Textures.Example1;
+
+public class Benzin {
+    long start, lifeTime;
+    public double x, y;
+    boolean create;
+    double initX, initY;
+    private final String firstPic="Balloon1.png";
+    public Benzin(){
+    }
+    public Benzin( double x, double y, int lifetime){
+        this.x = initX = x;
+        this.y = initY = y+110;
+        this.create = true;
+        start = System.currentTimeMillis();
+        lifeTime = lifetime;
+    }
+    public void validate(){
+        create = start+lifeTime > System.currentTimeMillis();
+    }
+    public String getFirstPic() {
+        return firstPic;
+    }
+
+
+}
+
+
